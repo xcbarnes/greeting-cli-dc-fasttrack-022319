974e3d967fdd6d3e49fbb3e66d9076c6e8766d27
Xavier C Barnes <xcbarnes@gmail.com>
2019-02-24 15:44:06 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,12 @@
 # code the #greeting method here!
+
+def clean(input)
+  clean_input = input.strip
+  clean_input = input.strip
+  return clean_input
+end
+
+def greeting(name)
+  name = clean(name)
+  puts "Hello #{name}. It's nice to meet you."
+end
